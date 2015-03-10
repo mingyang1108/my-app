d669c9d2bcc6c4b13722e3a5483a91726850d4d8
mingyang <mingyang1108@gmail.com>
2015-03-10 15:19:44 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,48 @@
+jsonp_handler({
+ "version": "1",
+ "build": 2,
+ "title": "my-app",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/mingyang1108/my-app/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  547,
+  64,
+  652507,
+  145175,
+  277980,
+  389,
+  35607,
+  113502,
+  97542
+ ],
+ "filedates": [
+  "2015-03-10T07:09:25.098Z",
+  "2015-03-10T07:09:25.097Z",
+  "2015-03-10T07:09:25.155Z",
+  "2015-03-10T07:09:51.418Z",
+  "2015-03-10T07:09:25.119Z",
+  "2015-03-10T07:09:25.120Z",
+  "2015-03-10T07:09:25.093Z",
+  "2015-03-10T07:09:25.085Z",
+  "2015-03-10T07:09:38.547Z"
+ ],
+ "date": "2015-03-10T07:09:52.387Z"
+})
